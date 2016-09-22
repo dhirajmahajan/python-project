05079ef82701e5c540a5e77deb2283c8b616dbe1
dhirajmahajan <dhirajmahajan03@gmail.com>
2016-09-22 20:55:29 +0930

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,10 @@
+__author__= 'dhiraj'
+
+def first_function():
+	massage = raw_input('Enter string=')
+	print type(massage)
+	print ('your massage' ,  massage)
+	print(' massage length' , len(massage) )
+
+if __name__ == "__main__":	
+	first_function()
